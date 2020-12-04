8a909ea74c92a3931f7afd4069054f9c20baa9b5
Brice <72446977+Brice-rm@users.noreply.github.com>
2020-12-04 11:00:30 +0100

    suite

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,81 @@
+<template>
+  <header class="header-global ">
+    <div style="position: relative">
+      <base-nav type="white" effect="light" expand>
+        <a class="navbar-brand" href="#"><img src="img/Life/logo life travel.png" id="logo"></a>
+
+        <div class="row" type="dark" slot="content-header" slot-scope="{ closeMenu }">
+          <div class="col-6 collapse-brand">
+            <img src="img/Life/logo life travel.png">
+          </div>
+          <div class="col-6 collapse-close">
+            <close-button @click="closeMenu"></close-button>
+          </div>
+        </div>
+
+        <ul class="navbar-nav ml-lg-auto ">
+          <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#"> ACCUEIL </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#"> PANIER </a>
+          </li>
+               <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#"> CONTACTS </a>
+          </li>
+          
+
+        </ul>
+      </base-nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown,
+  },
+};
+</script>
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  color: #4F555C;
+}
+p {
+   margin: 0;
+}
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!! */
+/* HEADER */
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!! */
+#logo {
+  height: 60px;
+  margin: 10px 0px 0px 10px;
+}
+
+@media screen and (min-width: 770px) and (max-width: 990px) {
+  #logo {
+    height: 60px;
+    margin: 5px 0px 0px 10px;
+  }
+}
+@media screen and (min-width: 991px) {
+  #logo {
+    height: 60px;
+    margin: 0px 0px 0px 15px;
+  }
+}
+.navbar{
+  z-index: 100;
+}
+</style>
